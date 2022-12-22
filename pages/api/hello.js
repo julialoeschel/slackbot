04276063d68311b40dd1799b24e12e3816935e6d
Julia Löschel <juloeschel@gmail.com>
2022-12-22 14:47:18 +0100

    initial refactor

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,57 +5,78 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 let payload = "";
 
 export default async function handler(req, res) {
-  const { response_url, text, channel_name } = req.body;
+  const { response_url, text, channel_name: courseName } = req.body;
 
   res.status(200).send();
-  execSync("mkdir -p tmp");
+  initializeTempDirectory();
 
-  const newFormatClone = execSync(
-    "git clone git@github.com:neuefische/web-curriculum-new-format.git",
-    { cwd: "tmp" }
-  );
+  try {
+    cloneReop("git@github.com:neuefische/web-curriculum-new-format.git");
 
-  const testClone = execSync(
-    `git clone git@github.com:shebtastic/${channel_name}.git`,
-    { cwd: "tmp" }
-  );
+    const [command, ...args] = text.trim().split(" ");
 
-  ///code goes here
+    switch (command) {
+      case "list": {
+        payload += listSessions();
+        break;
+      }
+      case "copy": {
+        const [sessionName] = args;
+        cloneReop(`git@github.com:shebtastic/${courseName}.git`);
+        payload += copySession(sessionName, courseName);
+        break;
+      }
+    }
 
-  if (text === "?list") {
+    const response = await fetch(response_url, {
+      method: "POST",
+      body: JSON.stringify({ text: payload, mrkdwn: true }),
+    });
+
+    console.log(response);
+  } finally {
+    deleteTempDirectory();
+  }
+
+  ///////////////////////
+  //functions
+  function initializeTempDirectory() {
+    execSync("mkdir -p tmp");
+  }
+
+  function deleteTempDirectory() {
+    execSync("rm -rf tmp");
+  }
+
+  function cloneReop(path, cwd = "tmp") {
+    execSync(`git clone ${path}`, { cwd });
+  }
+
+  function listSessions() {
     const ls = execSync("ls", {
       cwd: "tmp/web-curriculum-new-format/sessions",
     });
-    payload += ls.toString();
-  } else {
-    const args = text.split(" ");
-    if (args[0] === "copy") {
-      const cp = execSync(
-        `cp -r web-curriculum-new-format/sessions/${args[1]} ${channel_name}/sessions/.`,
-        {
-          cwd: `tmp`,
-        }
-      );
-      const push = execSync(
-        `git add .; git commit -m "add ${args[1]}" ; git push  `,
-        {
-          cwd: `tmp/${channel_name}`,
-        }
-      );
-      payload += `added *${args[1]}*`;
-    } else {
-      payload = "hallo";
-    }
+    return ls.toString();
   }
 
-  const response = await fetch(response_url, {
-    method: "POST",
-    body: JSON.stringify({ text: payload, mrkdwn: true }),
-  });
+  function copySession(
+    sessionName,
+    targetRepo,
+    sourceRepo = "web-curriculum-new-format"
+  ) {
+    const cp = execSync(
+      `cp -r ${sourceRepo}/sessions/${sessionName} ${targetRepo}/sessions/.`,
+      {
+        cwd: `tmp`,
+      }
+    );
 
-  console.log(response);
-
-  ///
-
-  execSync("rm -rf tmp");
+    const push = execSync(
+      `git add .; git commit -m "add ${sessionName}" ; git push  `,
+      {
+        cwd: `tmp/${targetRepo}`,
+      }
+    );
+    return `added *${sessionName}*`;
+  }
 }
